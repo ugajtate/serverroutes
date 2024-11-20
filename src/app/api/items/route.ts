914c27c072d645a8ac889b0160a2849a251fca7d
ugajtate <104660702+ugajtate@users.noreply.github.com>
2024-11-20 17:00:50 -0500

    Fixed file systems

--- src/app/api/items/route.ts
+++ src/app/api/items/route.ts
@@ -1,5 +1,5 @@
-import connectMongoDB from "../../../../libs/mongodb";
-import Item from "../../../models";
+import connectMongoDB from "../../libs/mongodb";
+import Item from "../../models/models";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
